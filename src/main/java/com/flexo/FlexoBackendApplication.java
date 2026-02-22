4df2e6dcadd05f51187b4b2168b2e119372b2a0d
PrzemCza <zamaskowany@o2.pl>
2026-02-22 21:03:28 +0100

    tekst testowy do CI/CD

--- src/main/java/com/flexo/FlexoBackendApplication.java
+++ src/main/java/com/flexo/FlexoBackendApplication.java
@@ -3,10 +3,13 @@ package com.flexo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class FlexoBackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Nowa wersja działa!");
 		SpringApplication.run(FlexoBackendApplication.class, args);
 	}
 
